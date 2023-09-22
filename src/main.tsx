048b3c15abf7379de80e94d86fbb5eb4351114ab
Nauman Javed <63299036+nomitoor@users.noreply.github.com>
2023-09-22 14:57:56 +0500

    Fix: Added tailwind and redesign the the links

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/index.css'
-import './styles/App.scss'
+// import './styles/App.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'kanit': ["'Kanit'", 'sans-serif'],
+                'work-sans': ["'Work Sans'", 'sans-serif']
+            },
+        },
+    },
+    plugins: [],
 }
-
